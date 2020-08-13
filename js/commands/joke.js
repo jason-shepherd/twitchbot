9efc67666857cf0e11a420db1b668147d8e4b272
DESKTOP-18UMFP0\C4xy <c4xy@outlook.com>
2020-08-13 20:27:54 +0100

    added comments to the joke command

--- js/commands/joke.js
+++ js/commands/joke.js
@@ -1,19 +1,37 @@
+const request = require("request");
+//TODO: Possibly add args to the command
+//TODO: Add line breaks
+//TODO: make it work
+//Sometimes just doesnt work
 exports.execute = (twitch, command, args, context, commands) => {
-    console.log("joke yes");
-    
-    var j = "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,racist,sexist";
-    var request = require('request');
-    request(j, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body)
-            console.log(body);
-            var setup = data.setup; var delivery = data.delivery
-            console.log(setup);  console.log(delivery);
-            var r = setup + "  " + delivery;
-            twitch.say(r);
-            console.log(r);
-        } else { console.log(error); }
-    })
-}
-//you touch this thing once and it break idk why 
-//TODO: clean up and make it function not bad yes
+    //url for api
+  var jokeapi =
+    "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,racist,sexist";
+    //request the webpage
+  request(jokeapi, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //get data
+      var data = JSON.parse(body); console.log(body);
+
+      //<--start setup joke-->
+      //set up variables for joke; each joke is seperated into two variables
+      var setup = ""; var delivery = "";
+      
+      //check if values are undefined and if they are define them 
+        setup = data.setup;
+        delivery = data.delivery;
+
+      //<--end setup joke-->
+
+      //tell the joke in twitch chat
+      if(setup == undefined || data == undefined) {
+          twitch.say('error');
+      } else {  twitch.say(`${setup} ${delivery}`);}
+      
+
+    } else {
+        //an error has occured
+      console.log(error); twitch.say(`an error has occured when attempting to get a joke :(`);
+    }
+  });
+};
