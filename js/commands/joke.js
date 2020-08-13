4874bc2e7c726d0013fd77cd33ee0b129fb62fe5
Jason Shepherd <jmanshepherd19@gmail.com>
2020-08-13 19:04:00 -0400

    can now choose category

--- js/commands/joke.js
+++ js/commands/joke.js
@@ -1,24 +1,28 @@
 //TODO: Possibly add args to the command
 
+const parser = require("../parseVariable");
 //request module used to get jokes from jokeapi
 const request = require("request");
-
-//jokeapi url
-const jokeapi = "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,racist,sexist";
 const options = {json: true};
 
+//jokeapi url
+const jokeapi = "https://sv443.net/jokeapi/v2/joke/{{catagories}}?blacklistFlags=nsfw,racist";
+let catagories = "Programming,Miscellaneous,Pun";
+
 exports.execute = (twitch, command, args, context, commands) => {
-    request(jokeapi, options, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            let joke = "";
-            //Some jokes are formatted differently, so we check which format it's using
-            if(body.joke === undefined)
-                joke = `${body.setup} ${body.delivery}`
-            else
-                joke = body.joke;
-            twitch.say(joke);
-        } else {
-            console.log(error); twitch.say(`An error has occured when attempting to get a joke :(. Please try again later.`);
-        }
-    });
+   if(args.length)
+      catagories = args;  
+   request(parser(jokeapi, {catagories: catagories}), options, (error, response, body) => {
+       if (!body.error && response.statusCode == 200) {
+           let joke = "";
+           //Some jokes are formatted differently, so we check which format it's using
+           if(body.joke === undefined)
+               joke = `${body.setup} ${body.delivery}`
+           else
+               joke = body.joke;
+           twitch.say(joke);
+       } else {
+           twitch.say(`${body.message} :(.`);
+       }
+   });
 };
