c784eb1fc318ea602c60c2b600c2567c6fd036ae
Jason Shepherd <jmanshepherd19@gmail.com>
2020-08-16 15:59:11 -0400

    removed a stray console.log

--- js/commands/help.js
+++ js/commands/help.js
@@ -1,6 +1,5 @@
 exports.execute = (twitch, command, args, context, commands) => {
     var helpText = "This bot currently supports the commands: ";
-    console.log(commands);
     Object.keys(commands).forEach(key => {
        if(key != "textresponse") {
             helpText += `!${key} `;
