c93445be36fcc97c0e1249e4dee3cef211648e36
iFroggo <c4xy@outlook.com>
2021-06-18 22:06:44 +0100

    25% base

--- js/commands/ship.js
+++ js/commands/ship.js
@@ -6,7 +6,11 @@ exports.execute = (twitch, command, context, commands) => {
 
     if(command.args[2] == "!") {
         var PersonA = checkForMention(command.args[0]); var PersonB = checkForMention(command.args[1]);
-        twitch.say(`Ship percentage between ${command.args[0]} and ${command.args[1]} is ${Math.round(50*(1/(Math.abs(PersonA.length - PersonB.length))) + 50*(1/(Math.abs((PersonA.toLowerCase().charCodeAt(0) - 96) - (PersonB.toLowerCase().charCodeAt(0) - 96)))))}%`);
+        //this can return infinity and also persona.length - personb.length can be 0 and then 1/0 * 50 = not poggers 
+        //but "i wouldnt care as long as it works" 
+        //somebody should probably make this not bad
+        //could just be a random number but "you teased the big brain formula now follow thru"
+        twitch.say(`Ship percentage between ${command.args[0]} and ${command.args[1]} is ${Math.round(50*(1/(Math.abs(PersonA.length - PersonB.length))) + 25*(1/(Math.abs((PersonA.toLowerCase().charCodeAt(0) - 96) - (PersonB.toLowerCase().charCodeAt(0) - 96)))))+25}%`);
     } else {
         twitch.say(`Ship percentage between ${command.args[0]} and ${command.args[1]} is ${Math.round(Math.random()*100)}%`);
     }
