98f6ed791ed52ba7775d5b0a519e42ed1b22aaa1
iFroggo <c4xy@outlook.com>
2021-06-18 20:46:15 +0100

    basic random ship command

--- js/commands/ship.js
+++ js/commands/ship.js
@@ -2,8 +2,8 @@
 exports.helpText = "Ship two people";
 
 exports.execute = (twitch, command, context, commands) => {
-    twitch.say("men");
-     
     
+    twitch.say(`Ship percentage between ${command.args[0]} and ${command.args[1]} is ${Math.floor(Math.random() * 100)}%`);
+
     return true;
 }
